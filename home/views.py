87d5adc0644049529057aa7b7dbb92447b56a795
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-05-07 14:35:48 +0200

    Create base template

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def homepage(request):
+    """
+    A view to return the index page.
+    """
+    return render(request, 'home/index.html')
